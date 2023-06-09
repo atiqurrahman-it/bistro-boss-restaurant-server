869b1d5b43fcca64f84625f1e825e43bce58ae79
atiqur <atiqurrahman.cse2552@gmail.com>
2023-06-10 02:16:40 +0600

    crete cart item show api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 let port = process.env.PORT || 5000;
 
@@ -29,6 +29,35 @@ async function run() {
 
     const menuCollection = client.db("BistroDB").collection("menu");
     const reviewCollection = client.db("BistroDB").collection("reviews");
+
+    const cartCollection = client.db("BistroDB").collection("carts");
+
+    // cart collection api
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+
+      const query = { userEmail:email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const item = req.body;
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/carts/:id",async (req,res)=>{
+      const id=req.params.id
+       const query = { _id: new ObjectId(id) };
+       const result = await cartCollection.deleteOne(query);
+       res.send(result)
+    })
+
+    //
 
     app.get("/menu", async (req, res) => {
       const menus = await menuCollection.find().toArray();
